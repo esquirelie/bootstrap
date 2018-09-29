7b73da19d1976b4eb00a60690204796fff64a19f
Enrique Canseco <lcanseco.enrique@gmail.com>
2018-09-28 21:46:43 -0500

    archivos base
    carga de archivos base

---
+++ hoja2.php
@@ -0,0 +1,55 @@
+ <!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 50px;
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+<?php 
+
+//Oracle DB user name
+$username = 'chimichanga';
+
+// Oracle DB user password
+$password = 'chimichanga';
+
+// Oracle DB connection string
+$connection_string = 'localhost/xe';
+
+//Connect to an Oracle database
+$connection = oci_connect(
+ $username,
+ $password,
+ $connection_string
+);
+
+If (!$connection)
+   echo 'Oops :( connection failed';
+else
+	$var =  ' Conexion de base de datos en Oracle Correcta!! ';
+// Close connection 
+?>
+    </head>
+    <body>
+			<?php include './idx_nav.html';	?>
+			<?php include './idx_jumbotron.html';	?>
+	
+    </body>
+</html>
